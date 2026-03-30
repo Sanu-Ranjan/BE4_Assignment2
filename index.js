d0136121f5878d3a40c59416b614d53fe92006f6
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-03-30 14:19:46 +0530

    feat: initialize express app with mongodb connection

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { connectDb } = require("./db/db.connection");
+const { Recipe } = require("./models/recipe.models");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+(async () => {
+  await connectDb();
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log("Server listening on port", PORT);
+  });
+})();
